38ea55f1b5fa54ba74842c4d9bca0322cbe0ae46
AnimuCat <50410152+AnimuCat@users.noreply.github.com>
2019-08-02 19:25:30 +0700

    Add files via upload

---
+++ Hexagon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon
+{
+    public class Hexagon
+    {
+        public int calculate_perimeter(int size)
+        {
+            int Perimeter;
+            return Perimeter = size * 6;
+        }
+
+        public int random_calculate_perimeter()
+        {
+            Random randSize = new Random();
+            int randSizeInt = randSize.Next(1, 999999);
+
+            return randSizeInt * 6;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            Hexagon hexagon = new Hexagon();
+
+            Console.WriteLine(hexagon.calculate_perimeter(size));
+            Console.WriteLine(hexagon.random_calculate_perimeter());
+            Console.ReadKey();
+        }
+    }
+}
